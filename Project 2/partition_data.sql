35d8b7a82e61b0228b3fae9b28dcb47c19ba0b84
Ronak Sumbaly <ronak.sumbaly@gmail.com>
2015-12-22 14:23:22 -0800

    Comments more added

--- Project 2/partition_data.sql
+++ Project 2/partition_data.sql
@@ -2,7 +2,6 @@
 -- This script partitions the initial database table (dataset) into two
 -- subsets: testset and trainset. It also verticalizes the data in the
 -- sense that it creates a record in testset or trainset for each column
--- in each record of dataset.
 -- run using (-td~ -vf) 
 
 ---- INITIALIZATION ----
@@ -15,20 +14,20 @@ DROP TABLE TRAIN_DATASET~
 
 -- create the tables
 
-CREATE TABLE TEST_DATASET("PID" INTEGER, "COLUMNNO" INTEGER, "ATT" VARCHAR(10),"DECISION" VARCHAR(10), "WEIGHT" INTEGER)~
-CREATE TABLE TRAIN_DATASET("PID" INTEGER, "COLUMNNO" INTEGER, "ATT" VARCHAR(10),"DECISION" VARCHAR(10), "WEIGHT" INTEGER)~
+CREATE TABLE TEST_DATASET("PID" INTEGER, "COLUMNNO" INTEGER, "ATT" VARCHAR(10),"DECISION" VARCHAR(10), "WEIGHT" INTEGER)~ -- CREATE TEST DATASET TABLE
+CREATE TABLE TRAIN_DATASET("PID" INTEGER, "COLUMNNO" INTEGER, "ATT" VARCHAR(10),"DECISION" VARCHAR(10), "WEIGHT" INTEGER)~ -- CREATE TRAIN DATASET TABLE
 
 -- transfer content to trainset and testset
 
 BEGIN ATOMIC
-	FOR temp AS SELECT * FROM DATASET ORDER BY PID DO
-	IF rand() > 0.75 THEN
+	FOR temp AS SELECT * FROM DATASET ORDER BY PID DO  --LOOP THROUGH ENTIRE DATASET
+	IF rand() > 0.75 THEN   -- SELECT RECORDS ON RANDOM AND ADD TO TEST IF < 75 %
 		INSERT INTO TEST_DATASET VALUES 
 		(temp.PID, 3, temp.sex, temp.survived, 1),
 		(temp.PID, 1, temp.class, temp.survived, 1),
 		(temp.PID, 2, temp.type, temp.survived, 1);
 		
-	ELSE
+	ELSE    -- SELECT RECORDS ON RANDOM AND ADD TO TRAIN
 		INSERT INTO TRAIN_DATASET VALUES 
 		(temp.PID, 3, temp.sex, temp.survived, 1),
 		(temp.PID, 1, temp.class, temp.survived, 1),
@@ -36,8 +35,6 @@ BEGIN ATOMIC
 	END IF;
 	END FOR;
 END~
-
----- CLEAN UP ----
 
 CONNECT RESET ~
 TERMINATE~
